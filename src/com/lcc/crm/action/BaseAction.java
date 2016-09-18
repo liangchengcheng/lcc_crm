b9e5bbc819e3570c761c8d871eec6f02d258d969
liangchengcheng <1038127753@qq.com>
2016-09-18 13:57:53 +0800

    add base action file
    add base action file

---
+++ src/com/lcc/crm/action/BaseAction.java
@@ -0,0 +1,36 @@
+package com.lcc.crm.action;
+
+import java.lang.reflect.ParameterizedType;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class BaseAction<T> extends ActionSupport implements ModelDriven<T> {
+
+	private Class clazz;
+	private T t;
+	
+	public BaseAction() {
+		try {
+			ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
+			clazz = (Class) type.getActualTypeArguments()[0];
+			this.t=(T) this.clazz.newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static final String LISTACTION="listaction";
+	public static final String ADDUI="addUI";
+	public static final String UPDATEUI="updateUI";
+	public static final String ACTION2ACTION="action2action";
+	public String listAction=LISTACTION;
+	public String updateUI=UPDATEUI;
+	public String addUI=ADDUI;
+	public String action2action=ACTION2ACTION;
+	@Override
+	public T getModel() {
+		return this.t;
+	}
+
+}
