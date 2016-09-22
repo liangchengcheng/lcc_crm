1b78f31ba6aab5336329ef059d8461c77222f96e
liangchengcheng <1038127753@qq.com>
2016-09-22 22:12:44 +0800

    xml set file
    xml set file

--- src/com/lcc/crm/action/CompanyAction.java
+++ src/com/lcc/crm/action/CompanyAction.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.struts2.ServletActionContext;
+import org.dom4j.datatype.DatatypeAttribute;
+import org.omg.PortableServer.ID_ASSIGNMENT_POLICY_ID;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
@@ -12,12 +15,14 @@ import com.lcc.crm.bean.CompanySearch;
 import com.lcc.crm.domain.Company;
 import com.lcc.crm.domain.SysDictionaryType;
 import com.lcc.crm.domain.SysUser;
+import com.lcc.crm.domain.SysUserGroup;
 import com.lcc.crm.service.ICityService;
 import com.lcc.crm.service.ICompanyService;
 import com.lcc.crm.service.IProvinceService;
 import com.lcc.crm.service.ISysDictionaryTypeService;
 import com.lcc.crm.service.ISysUserGroupService;
 import com.lcc.crm.service.ISysUserService;
+import com.lcc.crm.util.DataType;
 import com.lcc.crm.util.Global;
 import com.lcc.crm.util.SessionUtils;
 import com.opensymphony.xwork2.ActionContext;
@@ -85,6 +90,89 @@ public class CompanyAction extends BaseAction<Company> {
 			List<Company> companyList=this.companyService.findCompanysConition(sysUser, companySearch);
 			ActionContext.getContext().put("companyList", companyList);
 			return listAction;
+		}
+		return null;
+	}
+	
+	/**
+	 * 共享联系人
+	 * @return
+	 */
+	public String showShareSetOne(){
+		//获取客户的id
+		String sid= ServletActionContext.getRequest().getParameter("id");
+		if (StringUtils.isNotBlank(sid)) {
+			Integer id= Integer.parseInt(sid);
+			Company company =this.companyService.findCompanyById(id);
+			ActionContext.getContext().put("company", company);
+			//获取部门的信息
+			List<SysUserGroup> sysUserGroups=this.sysUserGroupService.findSysUserGroups();
+			ActionContext.getContext().put("sysUserGroups", sysUserGroups);
+			//获取用户的信息
+			List<SysUser> sysUsers=this.sysUserService.findAllSysUsers();
+			ActionContext.getContext().put("sysUsers", sysUsers);
+		}
+		
+		return "showShareSetOne";
+	}
+	
+	public String showShareCancelOne(){
+		//获取客户的id
+		String sid = ServletActionContext.getRequest().getParameter("id");
+		if (StringUtils.isNotBlank(sid.trim())) {
+			Integer id= Integer.parseInt(sid);
+			Company company=this.companyService.findCompanyById(id);
+			ActionContext.getContext().put("company", company);
+			return "showShareCancelOne";
+		}
+		return null;
+	}
+	
+	public String updateShareSetOne(){
+		//获取id
+		String sid= ServletActionContext.getRequest().getParameter("id");
+		if (StringUtils.isNotBlank(sid.trim())) {
+			Integer id = Integer.parseInt(sid);
+			//获取模板的名称
+			String s_module = ServletActionContext.getRequest().getParameter("s_module");
+			if (StringUtils.isNotBlank(s_module)) {
+				//获取用户的id
+				String[]suids= ServletActionContext.getRequest().getParameterValues("uid");
+				
+				Integer[]uids=DataType.convertStringArray2IntegerArray(suids);
+				String sharetyoe=ServletActionContext.getRequest().getParameter("sharetype");
+				if (StringUtils.isNotBlank(sharetyoe)) {
+					if ("add".equals(sharetyoe)) {
+						this.companyService.addUpdateShareSetOne(s_module, id, uids);
+					}
+					if ("minus".equals(sharetyoe)) {
+						this.companyService.minusUpdateShareSetOne(s_module, id, uids);
+					}
+				}
+				return "updateShareSetOne";
+			}
+		}
+		return null;
+	}
+	
+	public String showNextTouchTime(){
+		return "showNextTouchTime";
+	}
+	
+	/**
+	 * 更新下次的联系的时间
+	 */
+	public String updateNextTouchTime(){
+		//获取客户的id
+		String idString=ServletActionContext.getRequest().getParameter("ids");
+		if (StringUtils.isNotBlank(idString)) {
+			String[] sid=idString.split(",");
+			Integer[]id=DataType.convertStringArray2IntegerArray(sid);
+			//获取下次的联系的时间
+			String snext_touch_date=ServletActionContext.getRequest().getParameter("snext_touch_date");
+			java.sql.Date next_touch_date=java.sql.Date.valueOf(snext_touch_date);
+			this.companyService.updateNextTouchTime(id, next_touch_date);
+			return "updateNextTouchTime";
 		}
 		return null;
 	}
